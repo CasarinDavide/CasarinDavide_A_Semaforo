e63489e84ad29d351c9d06249a062aabea85a508
CasarinDavide <casarindavide345@gmail.com>
2019-02-06 13:39:01 +0100

    Semaforo Austriaco v2

--- SEMAFORO/SEMAFORO.ino
+++ SEMAFORO/SEMAFORO.ino
@@ -1,86 +1,106 @@
-int rosso1 = 5;
-int giallo1 = 6;
-int verde1= 7;
-int rosso2 = 10;
-int giallo2= 11;
-int verde2 = 12;
-void setup() {
+int rosso1 = 2 ;
+int giallo1 = 4 ;
+int verde1= 6 ;
+int rosso2 = 8 ;
+int giallo2= 9 ;
+int verde2= 10 ;
+int numLampeggi ;
+int DurataGiallo;
+int DurataRosso;
+int DurataVerde;
+int tempoLampeggi;
+void setup() 
+{
   // put your setup code here, to run once:
   pinMode (rosso1, OUTPUT);
   pinMode (giallo1, OUTPUT);
   pinMode (verde1,OUTPUT);
-  pinMode (rosso2, OUTPUT);
-  pinMode (giallo2, OUTPUT);
-  pinMode (verde2,OUTPUT);
-  
-  
+ 
 
 }
 
-void loop() {
+void loop() 
+{
   // put your main code here, to run repeatedly:
 digitalWrite(rosso1,HIGH);
-digitalWrite(verde2,HIGH);
 LampeggioVerde2();
-delay (2000);
-digitalWrite(giallo2,HIGH);
 digitalWrite(giallo1,HIGH);
-delay(2000);
-digitalWrite(giallo2,LOW);
+digitalWrite(giallo2 , HIGH);
 digitalWrite(rosso1,LOW);
-digitalWrite(rosso2,HIGH);
 digitalWrite(giallo1,LOW);
-digitalWrite(verde1,HIGH);
-delay(2000);
+digitalWrite(giallo2,LOW);
+digitalWrite(rosso2,HIGH);
 LampeggioVerde1();
 digitalWrite(giallo1,HIGH);
-delay(2000);
+digitalWrite(giallo2,HIGH); 
 digitalWrite(giallo1,LOW);
-digitalWrite(rosso1,LOW);
-
-
-
-
- 
+digitalWrite(giallo2,LOW);
+digitalWrite(rosso2,LOW);
 }
+
+void richiestaValoriLampeggi()   
+{
+  Serial.print ( "quanti lampeggi verdi?");
+  while ( Serial.available() == 0)   {};
+  numLampeggi  = Serial.readString().toInt();
+}
+
+
+
+void richiestaValoritempoLampeggi()   
+{
+  Serial.print ( "quanto tempo lampeggi verdi?");
+  while ( Serial.available() == 0)   {};
+  tempoLampeggi  = Serial.readString().toInt();
+}
+
+
+
+void richiestaDurataRosso()   
+{
+  Serial.print ( "quanto dura il rosso?");
+  while ( Serial.available() == 0)   {};
+  DurataRosso = Serial.readString().toInt();
+}
+
+
+
+void richiestaDuaratagiallo()   
+{
+  Serial.print ( "quanto dura il giallo?");
+  while ( Serial.available() == 0)   {};
+  DurataGiallo = Serial.readString().toInt();
+}
+
+
+
+void richiestaDuarataVerde()   
+{
+  Serial.print ( "quanto dura il verde?");
+  while ( Serial.available() == 0)   {};
+  DurataVerde  = Serial.readString().toInt();
+}
+
+
+
+
 void LampeggioVerde1(){
+  for (int i = 0; i<numLampeggi; i++)
+  {
   digitalWrite(verde1,HIGH);
-  delay(500);
   digitalWrite(verde1,LOW);
-  delay(500);
-  digitalWrite(verde1,HIGH);
-  delay(500);
-  digitalWrite(verde1,LOW);
-  delay(500);
-  digitalWrite(verde1,HIGH);
-  delay(500);
-  digitalWrite(verde1,LOW);
-  delay(500);
-  digitalWrite(verde1,HIGH);
-  delay(500);
-  digitalWrite(verde1,LOW);
-  
-
-
+  delay(tempoLampeggi);
+  }
 }
+
+
+
 void LampeggioVerde2(){
+  for (int i = 0; i< numLampeggi; i++)
+  {
   digitalWrite(verde2,HIGH);
-  delay(500);
   digitalWrite(verde2,LOW);
-  delay(500);
-  digitalWrite(verde2,HIGH);
-  delay(500);
-  digitalWrite(verde2,LOW);
-  delay(500);
-  digitalWrite(verde2,HIGH);
-  delay(500);
-  digitalWrite(verde2,LOW);
-  delay(500);
-digitalWrite(verde2,HIGH);
-  delay(500);
-  digitalWrite(verde2,LOW);
- 
-
-
-
+  delay(tempoLampeggi);
+  }
+  
 }
